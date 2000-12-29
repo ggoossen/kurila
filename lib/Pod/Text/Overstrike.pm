dcb3693a007fe91ed45a764b0c38d6170f8972e8
jhi <jhi@alpha>
2000-12-29 19:27:30 +0100

    Change 8259: Subject: podlators 1.06 released 	From: Russ Allbery <rra@stanford.edu> 	Date: 25 Dec 2000 05:09:30 -0800 	Message-ID: <ylwvcosl4l.fsf@windlord.stanford.edu>

--- lib/Pod/Text/Overstrike.pm
+++ lib/Pod/Text/Overstrike.pm
@@ -1,10 +1,10 @@
 # Pod::Text::Overstrike -- Convert POD data to formatted overstrike text
-# $Id: Overstrike.pm,v 1.10 2002/08/04 03:35:01 eagle Exp $
+# $Id: Overstrike.pm,v 1.1 2000/12/25 12:51:23 eagle Exp $
 #
 # Created by Joe Smith <Joe.Smith@inwap.com> 30-Nov-2000
 #   (based on Pod::Text::Color by Russ Allbery <rra@stanford.edu>)
 #
-# This program is free software; you may redistribute it and/or modify it
+# This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 #
 # This was written because the output from:
@@ -18,9 +18,9 @@
 # and because both Pod::Text::Color and Pod::Text::Termcap are not device
 # independent.
 
-##############################################################################
+############################################################################
 # Modules and declarations
-##############################################################################
+############################################################################
 
 package Pod::Text::Overstrike;
 
@@ -33,76 +33,57 @@ use vars qw(@ISA $VERSION);
 
 @ISA = qw(Pod::Text);
 
-# Don't use the CVS revision as the version, since this module is also in Perl
-# core and too many things could munge CVS magic revision strings.  This
-# number should ideally be the same as the CVS revision in podlators, however.
-$VERSION = 1.10;
+# Don't use the CVS revision as the version, since this module is also in
+# Perl core and too many things could munge CVS magic revision strings.
+# This number should ideally be the same as the CVS revision in podlators,
+# however.
+$VERSION = 1.01;
 
 
-##############################################################################
+############################################################################
 # Overrides
-##############################################################################
+############################################################################
 
 # Make level one headings bold, overridding any existing formatting.
 sub cmd_head1 {
-    my ($self, $text, $line) = @_;
-    $text =~ s/\s+$//;
-    $text = $self->strip_format ($self->interpolate ($text, $line));
-    $text =~ s/(.)/$1\b$1/g;
-    $self->SUPER::cmd_head1 ($text);
+    my $self = shift;
+    local $_ = shift;
+    s/\s+$//;
+    s/(.)\cH\1//g;
+    s/_\cH//g;
+    s/(.)/$1\b$1/g;
+    $self->SUPER::cmd_head1 ($_);
 }
 
 # Make level two headings bold, overriding any existing formatting.
 sub cmd_head2 {
-    my ($self, $text, $line) = @_;
-    $text =~ s/\s+$//;
-    $text = $self->strip_format ($self->interpolate ($text, $line));
-    $text =~ s/(.)/$1\b$1/g;
-    $self->SUPER::cmd_head2 ($text);
+    my $self = shift;
+    local $_ = shift;
+    s/\s+$//;
+    s/(.)\cH\1//g;
+    s/_\cH//g;
+    s/(.)/$1\b$1/g;
+    $self->SUPER::cmd_head2 ($_);
 }
 
 # Make level three headings underscored, overriding any existing formatting.
 sub cmd_head3 {
-    my ($self, $text, $line) = @_;
-    $text =~ s/\s+$//;
-    $text = $self->strip_format ($self->interpolate ($text, $line));
-    $text =~ s/(.)/_\b$1/g;
-    $self->SUPER::cmd_head3 ($text);
+    my $self = shift;
+    local $_ = shift;
+    s/\s+$//;
+    s/(.)\cH\1//g;
+    s/_\cH//g;
+    s/(.)/_\b$1/g;
+    $self->SUPER::cmd_head3 ($_);
 }
 
-# Level four headings look like level three headings.
-sub cmd_head4 {
-    my ($self, $text, $line) = @_;
-    $text =~ s/\s+$//;
-    $text = $self->strip_format ($self->interpolate ($text, $line));
-    $text =~ s/(.)/_\b$1/g;
-    $self->SUPER::cmd_head4 ($text);
-}
-
-# The common code for handling all headers.  We have to override to avoid
-# interpolating twice and because we don't want to honor alt.
-sub heading {
-    my ($self, $text, $line, $indent, $marker) = @_;
-    $self->item ("\n\n") if defined $$self{ITEM};
-    $text .= "\n" if $$self{loose};
-    my $margin = ' ' x ($$self{margin} + $indent);
-    $self->output ($margin . $text . "\n");
-}
-
-# Fix the various formatting codes.
-sub seq_b { local $_ = strip_format (@_); s/(.)/$1\b$1/g; $_ }
-sub seq_f { local $_ = strip_format (@_); s/(.)/_\b$1/g; $_ }
-sub seq_i { local $_ = strip_format (@_); s/(.)/_\b$1/g; $_ }
-
-# Output any included code in bold.
-sub output_code {
-    my ($self, $code) = @_;
-    $code =~ s/(.)/$1\b$1/g;
-    $self->output ($code);
-}
+# Fix the various interior sequences.
+sub seq_b { local $_ = $_[1]; s/(.)\cH\1//g; s/_\cH//g; s/(.)/$1\b$1/g; $_ }
+sub seq_f { local $_ = $_[1]; s/(.)\cH\1//g; s/_\cH//g; s/(.)/_\b$1/g; $_ }
+sub seq_i { local $_ = $_[1]; s/(.)\cH\1//g; s/_\cH//g; s/(.)/_\b$1/g; $_ }
 
 # We unfortunately have to override the wrapping code here, since the normal
-# wrapping code gets really confused by all the backspaces.
+# wrapping code gets really confused by all the escape sequences.
 sub wrap {
     my $self = shift;
     local $_ = shift;
@@ -110,12 +91,8 @@ sub wrap {
     my $spaces = ' ' x $$self{MARGIN};
     my $width = $$self{width} - $$self{MARGIN};
     while (length > $width) {
-        # This regex represents a single character, that's possibly underlined
-        # or in bold (in which case, it's three characters; the character, a
-        # backspace, and a character).  Use [^\n] rather than . to protect
-        # against odd settings of $*.
-        my $char = '(?:[^\n][\b])?[^\n]';
-        if (s/^((?>$char){0,$width})(?:\Z|\s+)//) {
+        if (s/^((?:(?:[^\n]\cH)?[^\n]){0,$width})\s+//
+            || s/^((?:(?:[^\n]\cH)?[^\n]){$width})//) {
             $output .= $spaces . $1 . "\n";
         } else {
             last;
@@ -126,22 +103,9 @@ sub wrap {
     $output;
 }
 
-##############################################################################
-# Utility functions
-##############################################################################
-
-# Strip all of the formatting from a provided string, returning the stripped
-# version.
-sub strip_format {
-    my ($self, $text) = @_;
-    $text =~ s/(.)[\b]\1/$1/g;
-    $text =~ s/_[\b]//g;
-    return $text;
-}
-
-##############################################################################
+############################################################################
 # Module return value and documentation
-##############################################################################
+############################################################################
 
 1;
 __END__
@@ -186,23 +150,11 @@ There may be some better approach possible.
 
 =head1 SEE ALSO
 
-L<Pod::Text>, L<Pod::Parser>
-
-The current version of this module is always available from its web site at
-L<http://www.eyrie.org/~eagle/software/podlators/>.  It is also part of the
-Perl core distribution as of 5.6.0.
+L<Pod::Text|Pod::Text>, L<Pod::Parser|Pod::Parser>
 
 =head1 AUTHOR
 
-Joe Smith <Joe.Smith@inwap.com>, using the framework created by Russ Allbery
-<rra@stanford.edu>.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2000 by Joe Smith <Joe.Smith@inwap.com>.
-Copyright 2001 by Russ Allbery <rra@stanford.edu>.
-
-This program is free software; you may redistribute it and/or modify it
-under the same terms as Perl itself.
+Joe Smith E<lt>Joe.Smith@inwap.comE<gt>, using the framework created by Russ
+Allbery E<lt>rra@stanford.eduE<gt>.
 
 =cut
