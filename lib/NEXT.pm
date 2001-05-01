66359a58bd7b97b954a67cd9aa4af1ee1afb162b
jhi <jhi@alpha>
2001-05-01 20:34:09 +0100

    Change 9938: Add NEXT, a pseudo-class for method redispatching. 	<Damian>NEXT.pm probably offers more bang-for-buck 	than anything else I've ever written.</Damian>

--- lib/NEXT.pm
+++ lib/NEXT.pm
@@ -1,14 +1,13 @@
 package NEXT;
-$VERSION = '0.50';
 use Carp;
 use strict;
 
 sub ancestors
 {
-	my @inlist = shift;
+	my @inlist = @_;
 	my @outlist = ();
-	while (my $next = shift @inlist) {
-		push @outlist, $next;
+	while (@inlist) {
+		push @outlist, shift @inlist;
 		no strict 'refs';
 		unshift @inlist, @{"$outlist[-1]::ISA"};
 	}
@@ -26,48 +25,25 @@ sub AUTOLOAD
 	croak "Can't call $wanted from $caller"
 		unless $caller_method eq $wanted_method;
 
-	local ($NEXT::NEXT{$self,$wanted_method}, $NEXT::SEEN) =
-	      ($NEXT::NEXT{$self,$wanted_method}, $NEXT::SEEN);
+	local $NEXT::NEXT{$self,$wanted_method} =
+	      $NEXT::NEXT{$self,$wanted_method};
 
-
-	unless ($NEXT::NEXT{$self,$wanted_method}) {
-		my @forebears =
-			ancestors ref $self || $self, $wanted_class;
+	unless (@{$NEXT::NEXT{$self,$wanted_method}||[]}) {
+		my @forebears = ancestors ref $self;
 		while (@forebears) {
 			last if shift @forebears eq $caller_class
 		}
 		no strict 'refs';
 		@{$NEXT::NEXT{$self,$wanted_method}} = 
-			map { *{"${_}::$caller_method"}{CODE}||() } @forebears
-				unless $wanted_method eq 'AUTOLOAD';
+			map { *{"${_}::$caller_method"}{CODE}||() } @forebears;
 		@{$NEXT::NEXT{$self,$wanted_method}} = 
-			map { (*{"${_}::AUTOLOAD"}{CODE}) ? "${_}::AUTOLOAD" : ()} @forebears
-				unless @{$NEXT::NEXT{$self,$wanted_method}||[]};
+			map { *{"${_}::AUTOLOAD"}{CODE}||() } @forebears
+				unless @{$NEXT::NEXT{$self,$wanted_method}};
 	}
-	my $call_method = shift @{$NEXT::NEXT{$self,$wanted_method}};
-	while ($wanted_class =~ /^NEXT:.*:UNSEEN/ && defined $call_method
-	       && $NEXT::SEEN->{$self,$call_method}++) {
-		$call_method = shift @{$NEXT::NEXT{$self,$wanted_method}};
-	}
-	unless (defined $call_method) {
-		return unless $wanted_class =~ /^NEXT:.*:ACTUAL/;
-		(local $Carp::CarpLevel)++;
-		croak qq(Can't locate object method "$wanted_method" ),
-		      qq(via package "$caller_class");
-	};
-	return shift()->$call_method(@_) if ref $call_method eq 'CODE';
-	no strict 'refs';
-	($wanted_method=${$caller_class."::AUTOLOAD"}) =~ s/.*:://
-		if $wanted_method eq 'AUTOLOAD';
-	$$call_method = $caller_class."::NEXT::".$wanted_method;
-	return $call_method->(@_);
+	$wanted_method = shift @{$NEXT::NEXT{$self,$wanted_method}};
+	return shift()->$wanted_method(@_) if $wanted_method;
+	return;
 }
-
-no strict 'vars';
-package NEXT::UNSEEN;		@ISA = 'NEXT';
-package NEXT::ACTUAL;		@ISA = 'NEXT';
-package NEXT::ACTUAL::UNSEEN;	@ISA = 'NEXT';
-package NEXT::UNSEEN::ACTUAL;	@ISA = 'NEXT';
 
 1;
 
@@ -80,36 +56,36 @@ NEXT.pm - Provide a pseudo-class NEXT that allows method redispatch
 
 =head1 SYNOPSIS
 
-    use NEXT;
+	use NEXT;
 
-    package A;
-    sub A::method   { print "$_[0]: A method\n";   $_[0]->NEXT::method() }
-    sub A::DESTROY  { print "$_[0]: A dtor\n";     $_[0]->NEXT::DESTROY() }
+	package A;
+	sub A::method   { print "$_[0]: A method\n";   $_[0]->NEXT::method() }
+	sub A::DESTROY  { print "$_[0]: A dtor\n";     $_[0]->NEXT::DESTROY() }
 
-    package B;
-    use base qw( A );
-    sub B::AUTOLOAD { print "$_[0]: B AUTOLOAD\n"; $_[0]->NEXT::AUTOLOAD() }
-    sub B::DESTROY  { print "$_[0]: B dtor\n";     $_[0]->NEXT::DESTROY() }
+	package B;
+	use base qw( A );
+	sub B::AUTOLOAD { print "$_[0]: B AUTOLOAD\n"; $_[0]->NEXT::AUTOLOAD() }
+	sub B::DESTROY  { print "$_[0]: B dtor\n";     $_[0]->NEXT::DESTROY() }
 
-    package C;
-    sub C::method   { print "$_[0]: C method\n";   $_[0]->NEXT::method() }
-    sub C::AUTOLOAD { print "$_[0]: C AUTOLOAD\n"; $_[0]->NEXT::AUTOLOAD() }
-    sub C::DESTROY  { print "$_[0]: C dtor\n";     $_[0]->NEXT::DESTROY() }
+	package C;
+	sub C::method   { print "$_[0]: C method\n";   $_[0]->NEXT::method() }
+	sub C::AUTOLOAD { print "$_[0]: C AUTOLOAD\n"; $_[0]->NEXT::AUTOLOAD() }
+	sub C::DESTROY  { print "$_[0]: C dtor\n";     $_[0]->NEXT::DESTROY() }
 
-    package D;
-    use base qw( B C );
-    sub D::method   { print "$_[0]: D method\n";   $_[0]->NEXT::method() }
-    sub D::AUTOLOAD { print "$_[0]: D AUTOLOAD\n"; $_[0]->NEXT::AUTOLOAD() }
-    sub D::DESTROY  { print "$_[0]: D dtor\n";     $_[0]->NEXT::DESTROY() }
+	package D;
+	use base qw( B C );
+	sub D::method   { print "$_[0]: D method\n";   $_[0]->NEXT::method() }
+	sub D::AUTOLOAD { print "$_[0]: D AUTOLOAD\n"; $_[0]->NEXT::AUTOLOAD() }
+	sub D::DESTROY  { print "$_[0]: D dtor\n";     $_[0]->NEXT::DESTROY() }
 
-    package main;
+	package main;
 
-    my $obj = bless {}, "D";
+	my $obj = bless {}, "D";
 
-    $obj->method();		# Calls D::method, A::method, C::method
-    $obj->missing_method(); # Calls D::AUTOLOAD, B::AUTOLOAD, C::AUTOLOAD
+	$obj->method();		# Calls D::method, A::method, C::method
+	$obj->missing_method(); # Calls D::AUTOLOAD, B::AUTOLOAD, C::AUTOLOAD
 
-    # Clean-up calls D::DESTROY, B::DESTROY, A::DESTROY, C::DESTROY
+	# Clean-up calls D::DESTROY, B::DESTROY, A::DESTROY, C::DESTROY
 
 
 =head1 DESCRIPTION
@@ -119,14 +95,8 @@ that uses it. If a method C<m> calls C<$self->NEXT::m()>, the call to
 C<m> is redispatched as if the calling method had not originally been found.
 
 In other words, a call to C<$self->NEXT::m()> resumes the depth-first,
-left-to-right search of C<$self>'s class hierarchy that resulted in the
-original call to C<m>.
-
-Note that this is not the same thing as C<$self->SUPER::m()>, which 
-begins a new dispatch that is restricted to searching the ancestors
-of the current class. C<$self->NEXT::m()> can backtrack
-past the current class -- to look for a suitable method in other
-ancestors of C<$self> -- whereas C<$self->SUPER::m()> cannot.
+left-to-right search of parent classes that resulted in the original
+call to C<m>.
 
 A typical use would be in the destructors of a class hierarchy,
 as illustrated in the synopsis above. Each class in the hierarchy
@@ -141,150 +111,10 @@ particular call, it might choose to redispatch that call, in the
 hope that some other C<AUTOLOAD> (above it, or to its left) might
 do better.
 
-By default, if a redispatch attempt fails to find another method
-elsewhere in the objects class hierarchy, it quietly gives up and does
-nothing (but see L<"Enforcing redispatch">). This gracious acquiesence
-is also unlike the (generally annoying) behaviour of C<SUPER>, which
-throws an exception if it cannot redispatch.
-
 Note that it is a fatal error for any method (including C<AUTOLOAD>)
-to attempt to redispatch any method that does not have the
-same name. For example:
+to attempt to redispatch any method except itself. For example:
 
-        sub D::oops { print "oops!\n"; $_[0]->NEXT::other_method() }
-
-
-=head2 Enforcing redispatch
-
-It is possible to make C<NEXT> redispatch more demandingly (i.e. like
-C<SUPER> does), so that the redispatch throws an exception if it cannot
-find a "next" method to call.
-
-To do this, simple invoke the redispatch as:
-
-	$self->NEXT::ACTUAL::method();
-
-rather than:
-
-	$self->NEXT::method();
-
-The C<ACTUAL> tells C<NEXT> that there must actually be a next method to call,
-or it should throw an exception.
-
-C<NEXT::ACTUAL> is most commonly used in C<AUTOLOAD> methods, as a means to
-decline an C<AUTOLOAD> request, but preserve the normal exception-on-failure 
-semantics:
-
-	sub AUTOLOAD {
-		if ($AUTOLOAD =~ /foo|bar/) {
-			# handle here
-		}
-		else {  # try elsewhere
-			shift()->NEXT::ACTUAL::AUTOLOAD(@_);
-		}
-	}
-
-By using C<NEXT::ACTUAL>, if there is no other C<AUTOLOAD> to handle the
-method call, an exception will be thrown (as usually happens in the absence of
-a suitable C<AUTOLOAD>).
-
-
-=head2 Avoiding repetitions
-
-If C<NEXT> redispatching is used in the methods of a "diamond" class hierarchy:
-
-	#     A   B
-	#    / \ /
-	#   C   D
-	#    \ /
-	#     E
-
-	use NEXT;
-
-	package A;                 
-	sub foo { print "called A::foo\n"; shift->NEXT::foo() }
-
-	package B;                 
-	sub foo { print "called B::foo\n"; shift->NEXT::foo() }
-
-	package C; @ISA = qw( A );
-	sub foo { print "called C::foo\n"; shift->NEXT::foo() }
-
-	package D; @ISA = qw(A B);
-	sub foo { print "called D::foo\n"; shift->NEXT::foo() }
-
-	package E; @ISA = qw(C D);
-	sub foo { print "called E::foo\n"; shift->NEXT::foo() }
-
-	E->foo();
-
-then derived classes may (re-)inherit base-class methods through two or
-more distinct paths (e.g. in the way C<E> inherits C<A::foo> twice --
-through C<C> and C<D>). In such cases, a sequence of C<NEXT> redispatches
-will invoke the multiply inherited method as many times as it is
-inherited. For example, the above code prints:
-
-        called E::foo
-        called C::foo
-        called A::foo
-        called D::foo
-        called A::foo
-        called B::foo
-
-(i.e. C<A::foo> is called twice).
-
-In some cases this I<may> be the desired effect within a diamond hierarchy,
-but in others (e.g. for destructors) it may be more appropriate to 
-call each method only once during a sequence of redispatches.
-
-To cover such cases, you can redispatch methods via:
-
-        $self->NEXT::UNSEEN::method();
-
-rather than:
-
-        $self->NEXT::method();
-
-This causes the redispatcher to skip any classes in the hierarchy that it has
-already visited in an earlier redispatch. So, for example, if the
-previous example were rewritten:
-
-        package A;                 
-        sub foo { print "called A::foo\n"; shift->NEXT::UNSEEN::foo() }
-
-        package B;                 
-        sub foo { print "called B::foo\n"; shift->NEXT::UNSEEN::foo() }
-
-        package C; @ISA = qw( A );
-        sub foo { print "called C::foo\n"; shift->NEXT::UNSEEN::foo() }
-
-        package D; @ISA = qw(A B);
-        sub foo { print "called D::foo\n"; shift->NEXT::UNSEEN::foo() }
-
-        package E; @ISA = qw(C D);
-        sub foo { print "called E::foo\n"; shift->NEXT::UNSEEN::foo() }
-
-        E->foo();
-
-then it would print:
-        
-        called E::foo
-        called C::foo
-        called A::foo
-        called D::foo
-        called B::foo
-
-and omit the second call to C<A::foo>.
-
-Note that you can also use:
-
-        $self->NEXT::UNSEEN::ACTUAL::method();
-
-or:
-
-        $self->NEXT::ACTUAL::UNSEEN::method();
-
-to get both unique invocation I<and> exception-on-failure.
+	sub D::oops { print "oops!\n"; $_[0]->NEXT::other_method() }
 
 
 =head1 AUTHOR
@@ -304,6 +134,7 @@ Comment, suggestions, and patches welcome.
 
 =head1 COPYRIGHT
 
- Copyright (c) 2000-2001, Damian Conway. All Rights Reserved.
+ Copyright (c) 2000, Damian Conway. All Rights Reserved.
  This module is free software. It may be used, redistributed
-    and/or modified under the same terms as Perl itself.
+and/or modified under the terms of the Perl Artistic License
+     (see http://www.perl.com/perl/misc/Artistic.html)
