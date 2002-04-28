2bdbb9f09f0de544fc34ec8e363784d97dbf253d
jhi <jhi@alpha>
2002-04-28 19:23:52 +0100

    Change 16251: Subject: WinCE many fixes 	From: "Vadim Konovalov" <vkonovalov@peterstar.ru> 	Date: Sun, 28 Apr 2002 21:54:31 +0400 	Message-ID: <00bf01c1eedd$c0c62a00$d25cc3d9@vad>

--- wince/comp.pl
+++ wince/comp.pl
@@ -2,12 +2,21 @@
 
 helper script to make life for PerlCE easier.
 
-There are different modes for running this script:
+You need edit values for @defs array to reflect your changes and then do
+
+  perl comp.pl [any-additional-options]
+
+This will call
+  nmake -f Makefile.ce
+with most parameters overrided as you specified and additional options
+(such as build target) will also be prepended to command line to execute.
+
+There are also additional different modes for running this script:
   perl comp.pl --run [any-command-line-arguments]
 and
   perl comp.pl --do [any-command-line-arguments]
 and
-  perl comp.pl --copy pc:[pc-location] ce:[ce-location]
+  perl comp.pl --copy-pm pc:[pc-location] ce:[ce-location]
 
 --run executes this build of perl on CE device with arguments provided
 --run=test will display a predefined messagebox that say everything is ok.
@@ -15,7 +24,7 @@ and
 --do  Executes on local computer command that is presented by arguments
       immediately following after --do
       Most reason why you may want to execute script in this mode is that
-      arguments preprocessed to replace [p] occurrences into current perl
+      arguments preprocessed to replace [p] occurences into current perl
       location. Typically it is handy to run
   perl comp.pl --do cecopy pc:..\lib\Exporter.pm ce:[p]\lib
 
@@ -28,16 +37,32 @@ and
 
 =cut
 
+use Cwd;
 use strict;
-use Cross;
-use Config;
 
 # edit value of $inst_root variable to reflect your desired location of
 # built perl
-my $inst_root = $Config{prefix};
+my $inst_root = "\\Storage Card\\perl-tests\\perl\@16225";
+my @defs = (
+  "\"PV=\"",
+  "\"INST_VER=\"",
+  "\"INSTALL_ROOT=$inst_root\"",
+  "\"WCEROOT=$ENV{SDKROOT}\"",
+  "NTPERL=$^X", #todo: check version: this must be (almost?) current version
+  "\"CEPATH=$ENV{WCEROOT}\"",
+  "CELIBDLLDIR=d:\\personal\\pocketPC\\celib-palm-3.0",
+  "CECONSOLEDIR=d:\\personal\\pocketPC\\w32console",
+  "YES=/y",
+  "CFG=RELEASE",
+  "MACHINE=wince-mips-pocket-wce300",
+  "PERLCEDIR=".cwd,
+  #NIY "\"CECOPY=\$(NTPERL) \$(PERLCEDIR)\\$0 --copy=compact\"",
+  "\"CECOPY=\$(NTPERL) \$(PERLCEDIR)\\$0 --copy\"",
+);
 
 my %opts = (
-  # %known_opts enumerates allowed opts as well as specifies default and initial values
+  # %known_opts enumerates allowed opts as well as specifies default
+  #   and initial values
   my %known_opts = (
      'do' => '',
      'run' => '',
@@ -73,7 +98,9 @@ elsif ($opts{'copy'}) {
   system("cecopy",@ARGV);
 }
 else {
-  # todo
+  my $cmd = "nmake -f Makefile.ce @defs @ARGV";
+  print $cmd;
+  system($cmd);
 }
 
 
