f2afb02c989e91e3ea6649226cd22228a0f9f56a
stevep <stevep@stevep-mccoy>
2005-12-07 12:38:00 +0100

    Change 26291: Assimilate Pod-Simple-3.03 to the Perl core

---
+++ lib/Pod/Simple/t/other^test^lib/hink^honk/Vliff.pm
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Bad pathname: '//depot/perl/lib/Pod/Simple/t/other^test^lib/hink^honk/Vliff.pm'</title>
+<style_0 <!--
+BODY { background: #ffffff; color: #000000; }
+--></style>
+<base href="http://public.activestate.com/cgi-bin/perlbrowse/X">
+</head>
+<body>
+
+<h2>Bad pathname: '//depot/perl/lib/Pod/Simple/t/other^test^lib/hink^honk/Vliff.pm'</h2>
+
+<p>[<a href="http://public.activestate.com/cgi-bin/perlbrowse">main page</a>]</body></html>
