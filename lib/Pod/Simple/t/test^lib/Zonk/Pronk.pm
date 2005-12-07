90d03f24678be24002a23df618c7eb67f4a337a9
stevep <stevep@stevep-mccoy>
2005-12-07 11:38:00 +0000

    Change 26291: Assimilate Pod-Simple-3.03 to the Perl core

---
+++ lib/Pod/Simple/t/test^lib/Zonk/Pronk.pm
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Bad pathname: '//depot/perl/lib/Pod/Simple/t/test^lib/Zonk/Pronk.pm'</title>
+<style_0 <!--
+BODY { background: #ffffff; color: #000000; }
+--></style>
+<base href="http://public.activestate.com/cgi-bin/perlbrowse/X">
+</head>
+<body>
+
+<h2>Bad pathname: '//depot/perl/lib/Pod/Simple/t/test^lib/Zonk/Pronk.pm'</h2>
+
+<p>[<a href="http://public.activestate.com/cgi-bin/perlbrowse">main page</a>]</body></html>
