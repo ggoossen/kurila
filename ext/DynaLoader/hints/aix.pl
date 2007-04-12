be1950fc3808e47cba4ad2c3e3618ee5d6ac25c5
gsar <gsar@auger>
2007-04-12 14:26:00 +0200

    Change 4336: integrate cfgperl contents into mainline

--- ext/DynaLoader/hints/aix.pl
+++ ext/DynaLoader/hints/aix.pl
@@ -2,13 +2,9 @@
 use Config;
 if ($Config{libs} =~ /-lC/ && -f '/lib/libC.a') {
     $self->{CCFLAGS} = $Config{ccflags} . ' -DUSE_libC';
-    if (-f '/usr/vacpp/include/load.h') {
-	$self->{CCFLAGS} .= ' -DUSE_vacpp_load_h';
-    } elsif (-f '/usr/ibmcxx/include/load.h') {
-	$self->{CCFLAGS} .= ' -DUSE_ibmcxx_load_h';
+    if (-f '/usr/ibmcxx/include/load.h') {
+	$self->{CCFLAGS} .= ' -I/usr/ibmcxx/include -DUSE_load_h';
     } elsif (-f '/usr/lpp/xlC/include/load.h') {
-	$self->{CCFLAGS} .= ' -DUSE_xlC_load_h';
-    } elsif (-f '/usr/include/load.h') {
-	$self->{CCFLAGS} .= ' -DUSE_load_h';
+	$self->{CCFLAGS} .= ' -I/usr/lpp/xlC/include -DUSE_load_h';
     }
 }
