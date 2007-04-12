a981193e45a227382d9f50226038abddb825bcbb
gsar <gsar@sparc26>
2007-04-12 14:23:04 +0200

    Change 3430: additional cleanups for cygwin32 port 		From: "Fifer, Eric" <EFifer@sanwaint.com> 		Date: Wed, 12 May 1999 20:25:54 +0100 		Message-ID: <71E287AB0D94D111BBD600600849EC8185EDAF@POST> 		Subject: [PATCH]perl5.005_03 (CORE) cygwin32 port

--- lib/ExtUtils/MM_Cygwin.pm
+++ lib/ExtUtils/MM_Cygwin.pm
@@ -1,32 +1,37 @@
 package ExtUtils::MM_Cygwin;
 
-use strict;
-use vars qw($VERSION @ISA);
-
 use Config;
-use File::Spec;
+#use Cwd;
+#use File::Basename;
+require Exporter;
 
-require ExtUtils::MM_Any;
-require ExtUtils::MM_Unix;
-@ISA = qw( ExtUtils::MM_Any ExtUtils::MM_Unix );
+Exporter::import('ExtUtils::MakeMaker',
+       qw( $Verbose &neatvalue));
 
-$VERSION = 1.04;
+unshift @MM::ISA, 'ExtUtils::MM_Cygwin';
+
+sub canonpath {
+    my($self,$path) = @_;
+    $path =~ s|\\|/|g;
+    return $self->ExtUtils::MM_Unix::canonpath($path);
+}
 
 sub cflags {
     my($self,$libperl)=@_;
     return $self->{CFLAGS} if $self->{CFLAGS};
-    return '' unless $self->needs_linking();
-
-    my $base = $self->SUPER::cflags($libperl);
+    my $base =$self->ExtUtils::MM_Unix::cflags($libperl);
     foreach (split /\n/, $base) {
-        /^(\S*)\s*=\s*(\S*)$/ and $self->{$1} = $2;
+      / *= */ and $self->{$`} = $';
     };
     $self->{CCFLAGS} .= " -DUSEIMPORTLIB" if ($Config{useshrplib} eq 'true');
+    $self->{CCFLAGS} .= " -DCYGWIN32" unless ($self->{CCFLAGS} =~ /\-DCYGWIN32/);
 
     return $self->{CFLAGS} = qq{
 CCFLAGS = $self->{CCFLAGS}
 OPTIMIZE = $self->{OPTIMIZE}
 PERLTYPE = $self->{PERLTYPE}
+LARGE = $self->{LARGE}
+SPLIT = $self->{SPLIT}
 };
 
 }
@@ -38,9 +43,9 @@ sub manifypods {
     my($dist);
     my($pod2man_exe);
     if (defined $self->{PERL_SRC}) {
-        $pod2man_exe = File::Spec->catfile($self->{PERL_SRC},'pod','pod2man');
+        $pod2man_exe = $self->catfile($self->{PERL_SRC},'pod','pod2man');
     } else {
-        $pod2man_exe = File::Spec->catfile($Config{scriptdirexp},'pod2man');
+        $pod2man_exe = $self->catfile($Config{scriptdirexp},'pod2man');
     }
     unless ($self->perl_script($pod2man_exe)) {
         # No pod2man but some MAN3PODS to be installed
@@ -52,40 +57,30 @@ Warning: I could not locate your pod2man program. Please make sure,
 END
         $pod2man_exe = "-S pod2man";
     }
-    my(@m) = ();
+    my(@m);
     push @m,
 qq[POD2MAN_EXE = $pod2man_exe\n],
 qq[POD2MAN = \$(PERL) -we '%m=\@ARGV;for (keys %m){' \\\n],
 q[-e 'next if -e $$m{$$_} && -M $$m{$$_} < -M $$_ && -M $$m{$$_} < -M "],
  $self->{MAKEFILE}, q[";' \\
 -e 'print "Manifying $$m{$$_}\n"; $$m{$$_} =~ s/::/./g;' \\
--e 'system(qq[$(PERLRUN) $(POD2MAN_EXE) ].qq[$$_>$$m{$$_}])==0 or warn "Couldn\\047t install $$m{$$_}\n";' \\
+-e 'system(qq[$$^X ].q["-I$(PERL_ARCHLIB)" "-I$(PERL_LIB)" $(POD2MAN_EXE) ].qq[$$_>$$m{$$_}])==0 or warn "Couldn\\047t install $$m{$$_}\n";' \\
 -e 'chmod(oct($(PERM_RW))), $$m{$$_} or warn "chmod $(PERM_RW) $$m{$$_}: $$!\n";}'
 ];
     push @m, "\nmanifypods : pure_all ";
-    push @m, join " \\\n\t", keys %{$self->{MAN1PODS}},
-                             keys %{$self->{MAN3PODS}};
+    push @m, join " \\\n\t", keys %{$self->{MAN1PODS}}, keys %{$self->{MAN3PODS}};
 
     push(@m,"\n");
     if (%{$self->{MAN1PODS}} || %{$self->{MAN3PODS}}) {
-        grep { $self->{MAN1PODS}{$_} =~ s/::/./g } keys %{$self->{MAN1PODS}};
-        grep { $self->{MAN3PODS}{$_} =~ s/::/./g } keys %{$self->{MAN3PODS}};
         push @m, "\t$self->{NOECHO}\$(POD2MAN) \\\n\t";
         push @m, join " \\\n\t", %{$self->{MAN1PODS}}, %{$self->{MAN3PODS}};
     }
     join('', @m);
 }
 
-sub perl_archive {
-    if ($Config{useshrplib} eq 'true') {
-        my $libperl = '$(PERL_INC)' .'/'. "$Config{libperl}";
-        if( $] >= 5.007 ) {
-            $libperl =~ s/a$/dll.a/;
-        }
-        return $libperl;
-    } else {
-        return '$(PERL_INC)' .'/'. ("$Config{libperl}" or "libperl.a");
-    }
+sub perl_archive
+{
+ return '$(PERL_INC)' .'/'. ("$Config{libperl}" or "libperl.a");
 }
 
 1;
@@ -103,7 +98,7 @@ ExtUtils::MM_Cygwin - methods to override UN*X behaviour in ExtUtils::MakeMaker
 
 See ExtUtils::MM_Unix for a documentation of the methods provided there.
 
-=over 4
+=over
 
 =item canonpath
 
@@ -120,8 +115,3 @@ replaces strings '::' with '.' in man page names
 =item perl_archive
 
 points to libperl.a
-
-=back
-
-=cut
-
