f0a96245e2fee8e169e5d97edf5b7116d6f04a5e
gsar <gsar@sparc26>
2007-04-12 14:22:20 +0200

    Change 3131: integrate cfgperl changes into mainline

--- hints/rhapsody.sh
+++ hints/rhapsody.sh
@@ -1,77 +1,54 @@
 ##
 # Rhapsody (Mac OS X Server) hints
-# Wilfredo Sanchez <wsanchez@mit.edu>
-##
-
-##
-# Paths
-##
-
-# BSD paths
-case "$prefix" in
-'')	
-	prefix='/usr/local'; # Built-in perl uses /usr
-	siteprefix='/usr/local';
-	vendorprefix='/usr/local'; usevendorprefix='define';
-
-	# 4BSD uses ${prefix}/share/man, not ${prefix}/man.
-	# Don't put man pages in ${prefix}/lib; that's goofy.
-	man1dir="${prefix}/share/man/man1";
-	man3dir="${prefix}/share/man/man3";
-
-	# Where to put modules.
-	# Built-in perl uses /System/Library/Perl
-	privlib='/Local/Library/Perl';
-	sitelib='/Local/Library/Perl';
-	vendorlib='/Network/Library/Perl';
-	;;
-esac
-
-##
-# Tool chain settings
+# Wilfredo Sanchez <wsanchez@apple.com>
 ##
 
 # Since we can build fat, the archname doesn't need the processor type
 archname='rhapsody';
 
-# nm works.
-usenm='true';
-  
+# Perl5.003 precedes this platform
+d_bincompat3='undef';
+
 # Libc is in libsystem.
 libc='/System/Library/Frameworks/System.framework/System';
+
+# nm works.
+usenm='true';
 
 # Optimize.
 optimize='-O3';
 
-# XXX Unclear why we require -pipe and -fno-common here.
-ccflags="${ccflags} -pipe -fno-common"
+# We have a prototype for telldir.
+# We are not NeXTStep.
+ccflags="${ccflags} -pipe -fno-common -DHAS_TELLDIR_PROTOTYPE -UNeXT -U__NeXT__";
 
-# cpp-precomp is problematic.
-cppflags='-traditional-cpp';
+# Don't use /usr/local/lib; we may have junk there.
+libpth='/lib /usr/lib';
 
-# Shared library extension is .dylib.
-# Bundle extension is .bundle.
+# Shared library extension in .dylib.
+# Bundle extension in .bundle.
 ld='cc';
 so='dylib';
 dlext='bundle';
-dlsrc='dl_dyld.xs';
-usedl='define';
+dlsrc='dl_rhapsody.xs';
 cccdlflags='';
 lddlflags="${ldflags} -bundle -undefined suppress";
-ldlibpthname='DYLD_LIBRARY_PATH';
 useshrplib='true';
+libperl='Perl';
+framework_path='/System/Library/Frameworks/Perl.framework';
+base_address='0x4be00000';
 
-##
-# System libraries
-##
-  
+# 4BSD uses /usr/share/man, not /usr/man.
+# Don't put man pages in /usr/lib; that's goofy.
+man1dir='/usr/share/man/man1';
+man3dir='/usr/share/man/man3';
+
+# Where to put modules.
+privlib='/System/Library/Perl';
+sitelib='/Local/Library/Perl';
+
 # vfork works
 usevfork='true';
 
 # malloc works
 usemymalloc='n';
-
-# Case-insensitive filesystems don't get along with Makefile and
-# makefile in the same place.  Since Darwin uses GNU make, this dodges
-# the problem.
-firstmakefile=GNUmakefile;
