b8037ee20e1ab468031e291fb1eaf9358f80b192
gerard <gerard@tty.nl>
2007-05-21 17:06:47 +0200

    Normal function call in CGI::Carp

--- lib/CGI/Carp.pm
+++ lib/CGI/Carp.pm
@@ -483,10 +483,10 @@ sub set_die_handler {
     return $CGI::Carp::DIE_HANDLER;
 }
 
-sub confess { CGI::Carp::die Carp::longmess @_; }
-sub croak   { CGI::Carp::die Carp::shortmess @_; }
-sub carp    { CGI::Carp::warn Carp::shortmess @_; }
-sub cluck   { CGI::Carp::warn Carp::longmess @_; }
+sub confess { CGI::Carp::die(Carp::longmess(@_)); }
+sub croak   { CGI::Carp::die(Carp::shortmess(@_)); }
+sub carp    { CGI::Carp::warn(Carp::shortmess(@_)); }
+sub cluck   { CGI::Carp::warn(Carp::longmess(@_)); }
 
 # We have to be ready to accept a filehandle as a reference
 # or a string.
