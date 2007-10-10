db313e39bbff3ebdc079b872c76069f01438e913
gerard <gerard@tty.nl>
2007-10-10 15:23:51 +0200

    improve compsub.pm documentation.

--- lib/compsub.pm
+++ lib/compsub.pm
@@ -90,44 +90,97 @@ it.
 =over 4
 
 =item Calling a subroutine or not depending on some global environment.
+This example creates a compilation sub C<debuglog>, which calls C<dolog> if C<$ENV{DEBUG}>
+is set. Thus checking for C<$ENV{DEBUG}> is done at compile time, and if 
+it not set no code is executed at run-time.
 
-sub dolog {
-    print $log, @_;
-}
 
-# this subroutines compiles when $ENV{DEBUG} is set to calling 'dolog' with its arguments,
-# otherwise it will do nothing (including NOT evaluting its arguments).
-sub compdebuglog {
-   my $op = shift;
-   if ($ENV{DEBUG}) {
-     my $cvop = B::SVOP->new('const', 0, *dolog);
-     $op ||= B::LISTOP->new('list', 0, $cvop, undef);
-     return B::UNOP->new('entersub', 0, $op);
-   } else {
-     $op && $op->free; # we don't use $op, so we must explicitly free it.
-     return B::OP->new('null', 0);
-   }
-}
-
-compsub::define( debuglog => \&compdebuglog );
-
-...
-
-debuglog Dump($complexvar);
+  sub dolog {
+      print $log, @_;
+  }
+  
+  # this subroutines compiles when $ENV{DEBUG} is set to calling 'dolog' with its arguments,
+  # otherwise it will do nothing (including NOT evaluting its arguments).
+  sub compdebuglog {
+     my $op = shift;
+     if ($ENV{DEBUG}) {
+       my $cvop = B::SVOP->new('const', 0, *dolog);
+       $op = B::LISTOP->new('list', 0, ($op ? ($op, $cvop) : ($cvop, undef)));
+       return B::UNOP->new('entersub', B::OPf_STACKED|B::OPf_SPECIAL, $op);
+     } else {
+       $op && $op->free; # we don't use $op, so we must explicitly free it.
+       return B::OP->new('null', 0);
+     }
+  }
+  
+  compsub::define( debuglog => \&compdebuglog );
+  
+  ...
+  
+  debuglog Dump($complexvar);
 
 =item parsing arguments and declaring lexical variables
 
-sub compparseparam {
 
-}
+    # assumes argument like: 'foo' => \$foo, 'bar' => \$bar, { @_ }
+    sub parseparams {
+        my $values = pop @_;
+        while (my $name = shift @_) {
+            $_[0] = $values->{$name};
+            shift @_;
+        }
+    }
+
+    # assumes argument like C<'foo', 'bar', { @_ }>
+    # this will be converted like C<parseparams('foo', \(my $foo), 'bar', \(my $bar), { @_ })>
+    sub compparams {
+        my $op = shift;
+        $op or return B::UNOP->new('null', 0);
+        my $kid = $op->first;
+        while (ref $kid ne "B::NULL") {
+            if ($kid->name eq "const") {
+                # allocate a 'my' variable
+                my $targ = B::PAD::allocmy( '$' . ${ $kid->sv->object_2svref } );
+                # introduce the 'my' variable, and insert it into the list of argument.
+                my $padsv = B::OP->new('padsv', B::OPf_MOD);
+                $padsv->set_private(B::OPpLVAL_INTRO);
+                $padsv->set_targ($targ);
+                $padsv->set_sibling($kid->sibling);
+                $kid->set_sibling($padsv);
+
+                $kid = $padsv;
+            } elsif ($kid->name eq "list" or $kid->name eq "pushmark") {
+                # ignore
+            } elsif ($kid->name eq "anonhash") {
+                # ignore, assume it is the last item in the list.
+            } else {
+                die "Expected constant opcode but got " . $kid->name;
+            }
+            $kid = $kid->sibling;
+        }
+        my $cvop = B::SVOP->new('const', 0, *parseparams);
+        $op = B::LISTOP->new('list', 0, ($op ? ($op, $cvop) : ($cvop, undef)));
+        my $entersubop = B::UNOP->new('entersub', B::OPf_STACKED|B::OPf_SPECIAL, $op);
+        return $entersubop;
+    }
+
+    BEGIN {
+        compsub::define( params => \&compparams )
+    }
+
+    {
+        sub foobar {
+            params 'foo', 'bar', { @_ };
+            is $foo, 'foo-value', '$foo declared and initialized';
+            is $bar, 'bar-value';
+        }
+
+        foobar( foo => "foo-value", bar => "bar-value" );
+    }
 
 =back
 
 =back
-
-=head1 IMPLEMENTATION
-
-...
 
 =head1 AUTHOR
 
