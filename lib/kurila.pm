d430871cf4b47bb1bb6edbc8ad0c0b9d9c8cf0ec
gerard <gerard@tty.nl>
2007-11-13 17:11:33 +0100

    add kurila module for "use kurila x.y"

---
+++ lib/kurila.pm
@@ -0,0 +1,29 @@
+package kurila;
+
+our $VERSION = '1.4';
+
+$^V eq "kurila-$VERSION"
+  or die "Perl lib version (kurila-$VERSION) doesn't match executable version (" . $^V . ")";
+
+1;
+
+__END__
+
+=head1 NAME
+
+kurila - Perl Kurila
+
+=head1 SYNOPSIS
+
+  use kurila 1.4;
+
+=head1 DESCRIPTION
+
+This pragma indicates that your module requires Perl Kurila. When used
+with Perl 5 it will give an error.
+
+=head1 SEE ALSO
+
+L<kurilaintro>.
+
+=cut
