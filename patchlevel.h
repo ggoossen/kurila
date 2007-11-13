1c1ce1cccac46376e8019774a549f4b63e46bf8e
gerard <gerard@tty.nl>
2007-11-13 17:56:34 +0100

    increave version to 1.5

--- patchlevel.h
+++ patchlevel.h
@@ -15,7 +15,7 @@
 
 #define KURILA_REVISION "kurila"
 #define KURILA_VERSION 1
-#define KURILA_SUBVERSION 4
+#define KURILA_SUBVERSION 5
 
 /* Fake Perl version 5.10.0 */
 #define PERL_REVISION	5		/* age */
