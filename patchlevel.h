ccb2e0dc7831c603ffc804d768bfcee40ff1aa40
gerard <gerard@tty.nl>
2008-03-20 13:48:26 +0100

    increate version number to 1.9

--- patchlevel.h
+++ patchlevel.h
@@ -15,7 +15,7 @@
 
 #define KURILA_REVISION "kurila"
 #define KURILA_VERSION 1
-#define KURILA_SUBVERSION 8
+#define KURILA_SUBVERSION 9
 
 /* Fake Perl version 5.10.0 */
 #define PERL_REVISION	5		/* age */
