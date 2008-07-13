2d7e7035da28f45fcc494862de0bd66bbda7f114
Gerard Goossen <gerard@tty.nl>
2008-07-13 19:38:19 +0200

    fixes for stricter globs in IPC::Open[23]

--- lib/IPC/Open2.pm
+++ lib/IPC/Open2.pm
@@ -109,7 +109,6 @@ function is really just a wrapper around open3().
 require IPC::Open3;
 
 sub open2 {
-    local $Carp::CarpLevel = $Carp::CarpLevel + 1;
     return IPC::Open3::_open3('open2', scalar caller,
 				@_[1], @_[0], '>&STDERR', @_[[2 .. ((nelems @_)-1)]]);
 }
