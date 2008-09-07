24c469f4f1bb56ea34ce3f1f3c9536b499cc0c2f
Gerard Goossen <gerard@tty.nl>
2008-09-07 19:15:15 +0200

    language mixing

--- mg.c
+++ mg.c
@@ -2671,9 +2671,9 @@ Perl_sighandler(int sig)
         flags |= 16;
 
     if (!PL_psig_ptr[sig]) {
-	# PerlIO_printf(Perl_error_log, "Signal SIG%s received, but no signal handler set.\n",
-	#     PL_sig_name[sig]);
-	# exit(sig);
+/* 	PerlIO_printf(Perl_error_log, "Signal SIG%s received, but no signal handler set.\n", */
+/* 	    PL_sig_name[sig]); */
+/* 	exit(sig); */
 	return;
     }
 
