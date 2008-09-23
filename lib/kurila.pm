01267f64644b969a7489896c716914a3c950bcd8
Gerard Goossen <gerard@tty.nl>
2008-09-23 12:33:26 +0200

    increase version number in "kurila.pm"

--- lib/kurila.pm
+++ lib/kurila.pm
@@ -1,9 +1,9 @@
 package kurila;
 
-our $VERSION = v1.13;
+our $VERSION = v1.14;
 
-$^V eq "kurila-1.13"
-  or die "Perl lib version (kurila-1.13) doesn't match executable version (" . $^V . ")";
+$^V eq "kurila-1.14"
+  or die "Perl lib version (kurila-1.14) doesn't match executable version (" . $^V . ")";
 
 1;
 
@@ -15,7 +15,7 @@ kurila - Perl Kurila
 
 =head1 SYNOPSIS
 
-  use kurila v1.13;
+  use kurila v1.14;
 
 =head1 DESCRIPTION
 
