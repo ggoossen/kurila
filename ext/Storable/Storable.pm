0f85bdc83e6ed3b07c9d9d931638291e19ede64f
Gerard Goossen <gerard@tty.nl>
2008-11-03 19:27:24 +0100

    correctly read config for flock in Storable

--- ext/Storable/Storable.pm
+++ ext/Storable/Storable.pm
@@ -80,16 +80,14 @@ sub logcarp {
 # Determine whether locking is possible, but only when needed.
 #
 
-our %Config;
-
 my $CAN_FLOCK;
 sub CAN_FLOCK {
 	return $CAN_FLOCK if defined $CAN_FLOCK;
-	require Config; Config->import;
+	require Config;
 	return $CAN_FLOCK =
-		%Config{'d_flock'} ||
-		%Config{'d_fcntl_can_lock'} ||
-		%Config{'d_lockf'};
+		Config::config_value('d_flock') ||
+		Config::config_value('d_fcntl_can_lock') ||
+		Config::config_value('d_lockf');
 }
 
 sub show_file_magic {
