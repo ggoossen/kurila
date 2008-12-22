494acf44bab7743cfaf0957dac2bfee48b2d6ebf
Gerard Goossen <gerard@tty.nl>
2008-12-22 15:26:42 +0100

    add restore function to env::

--- lib/env.pm
+++ lib/env.pm
@@ -2,10 +2,32 @@ package env;
 
 sub temp_set_var {
     my @($key, $value) = @_;
-    my $oldvalue = var($key);
-    push dynascope->{parent}->{onleave},
-      sub { set_var($key, $oldvalue) };
+    push dynascope->{parent}->{onleave}, make_restore_var($key);
     set_var($key, $value);
     return;
 }
+
+sub make_restore_var {
+    my @($key) = @_;
+    my $old_value = env::var($key);
+    my $restore_var = sub {
+        set_var($key, $old_value);
+    };
+    return $restore_var;
+}
+
+sub make_restore {
+    my %old = %:< map { $_ => env::var($_) } env::keys();
+    my $restore= sub {
+        my $old_copy = %old;
+        for my $key (env::keys()) {
+            env::set_var($key, delete $old_copy{$key});
+        }
+        for my $key (keys $old_copy) {
+            env::set_var($key, $old_copy{$key});
+        }
+    };
+    return $restore;
+}
+
 1;
