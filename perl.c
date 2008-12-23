838a18686dc3d0bdc00ef2f1d7d7e36b7f8510d8
Gerard Goossen <gerard@tty.nl>
2008-12-23 18:04:13 +0100

    fix -V option

--- perl.c
+++ perl.c
@@ -1524,14 +1524,14 @@ S_parse_body(pTHX_ char **env, XSINIT_t xsinit)
 #  endif
 #endif
 		    sv_catpvs(opts_prog, "; $\"=\"\\n    \"; "
-			      "our @env = map { \"$_=\\\"%ENV{$_}\\\"\" } "
-			      "sort grep {m/^PERL/} keys %ENV; ");
+			      "our @env = map { \"$_=\\\"$(env::var($_))\\\"\" } "
+			      "sort grep {m/^PERL/} env::keys; ");
 #ifdef __CYGWIN__
 		    sv_catpvs(opts_prog,
-			      "push @env, \"CYGWIN=\\\"%ENV{CYGWIN}\\\"\";");
+			      "push @env, \"CYGWIN=\\\"$(env::var('CYGWIN'))\\\"\";");
 #endif
 		    sv_catpvs(opts_prog, 
-			      "print \"  \\%ENV:\\n    $(join ' ', @env)\\n\" if @env;"
+			      "print \"  env:\\n    $(join ' ', @env)\\n\" if @env;"
 			      "print \"  \\@INC:\\n    $(join ' ', @INC)\\n\";");
 		}
 		else {
