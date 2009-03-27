0554f75a7e4f9aae7aa4eab4151e489a05231b4d
Gerard Goossen <gerard@tty.nl>
2009-03-28 00:18:32 +0100

    allow assignments to assignments

--- op.c
+++ op.c
@@ -2580,12 +2580,6 @@ Perl_newASSIGNOP(pTHX_ OPFLAGS flags, OP *left, I32 optype, OP *right, SV *locat
     if (optype) {
 	bool is_logassign = (optype == OP_ANDASSIGN || optype == OP_ORASSIGN || optype == OP_DORASSIGN);
 
-	if ( left->op_type == OP_SASSIGN || left->op_type == OP_ANDASSIGN
-	    || left->op_type == OP_ORASSIGN || left->op_type == OP_DORASSIGN ) {
-	    Perl_croak_at(aTHX_ left->op_location, "Can't do %s to %s", 
-		PL_op_desc[optype], OP_DESC(left));
-	}
-
 	if (is_logassign) {
 	    OP* new_left = left;
 	    OP* finish_assign = op_assign(&new_left, optype);
--- t/comp/assignment.t
+++ t/comp/assignment.t
@@ -4,11 +4,8 @@ BEGIN {
     require './test.pl';
 }
 
-plan( tests => 2 );
+plan( tests => 1 );
 
 my ($a, $b, $c);
-$a = $: $b = 3; 
+$a = $: $b = 3;
 is( $a, 3);
-
-eval_dies_like( '$a = $b ||= $c',
-                qr/Can't do logical or assignment [(]||=[)] to assignment/ );
