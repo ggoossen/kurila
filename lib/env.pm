a6e72e3984112cd36311fc47b4072b1be08710a8
Gerard Goossen <gerard@tty.nl>
2009-06-12 18:46:45 +0200

    regen pods

--- lib/env.pm
+++ lib/env.pm
@@ -6,13 +6,9 @@ sub make_restore
         my $old_copy = %old
         for my $key (env::keys())
             env::var($key) = delete $old_copy{$key}
-        
         for my $key (CORE::keys $old_copy)
             env::var($key) = $old_copy{$key}
-        
         return
-    
     return $restore
-
 
 1
