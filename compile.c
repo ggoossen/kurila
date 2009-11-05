6b631f83af60e42d8c319df157c2e2de13cc6e89
Gerard Goossen <gerard@ggoossen.net>
2009-11-05 12:18:47 +0100

    refactoring compile

--- compile.c
+++ compile.c
@@ -20,6 +20,7 @@ struct op_instrpp {
 typedef struct op_instrpp OP_INSTRPP;
 
 struct branch_point_pad {
+    int idx;
     OP_INSTRPP* op_instrpp_compile;
     OP_INSTRPP* op_instrpp_list;
     OP_INSTRPP* op_instrpp_end;
@@ -28,7 +29,7 @@ struct branch_point_pad {
 typedef struct branch_point_pad BRANCH_POINT_PAD;
 
 void
-    S_append_branch_point(pTHX_ BRANCH_POINT_PAD* bpp, OP* o, INSTRUCTION** instrp)
+S_append_branch_point(pTHX_ BRANCH_POINT_PAD* bpp, OP* o, INSTRUCTION** instrp)
 {
     DEBUG_x(Perl_deb("adding branch point "); dump_op_short(o); Perl_deb("\n"));
     if (bpp->op_instrpp_append >= bpp->op_instrpp_end) {
@@ -47,65 +48,55 @@ void
 }
 
 void
-Perl_compile_op(pTHX_ OP* startop, CODESEQ* codeseq)
+    S_append_instruction(pTHX_ CODESEQ* codeseq, BRANCH_POINT_PAD* bpp, OP* o, Optype optype)
 {
-    OP* o;
-    int idx = 0;
+    codeseq->xcodeseq_instructions[bpp->idx].instr_ppaddr = PL_ppaddr[optype];
+    codeseq->xcodeseq_instructions[bpp->idx].instr_op = o;
+    codeseq->xcodeseq_instructions[bpp->idx].instr_arg = NULL;
 
-    BRANCH_POINT_PAD bpp;
+    bpp->idx++;
+    if (bpp->idx >= codeseq->xcodeseq_size) {
+	codeseq->xcodeseq_size += 128;
+	Renew(codeseq->xcodeseq_instructions, codeseq->xcodeseq_size, INSTRUCTION);
+    }
 
-    Newx(bpp.op_instrpp_list, 128, OP_INSTRPP);
-    bpp.op_instrpp_compile = bpp.op_instrpp_list;
-    bpp.op_instrpp_append = bpp.op_instrpp_list;
-    bpp.op_instrpp_end = bpp.op_instrpp_list + 128;
+}
 
-    PERL_ARGS_ASSERT_COMPILE_OP;
-    codeseq->xcodeseq_size = 128;
-    Renew(codeseq->xcodeseq_instructions, codeseq->xcodeseq_size, INSTRUCTION);
-
-    o = startop;
-
-    do {
-	while (o) {
+void
+    S_add_op(CODESEQ* codeseq, BRANCH_POINT_PAD* bpp, OP* o)
+{
+    while (o) {
 	    DEBUG_x(Perl_deb("Compiling op "); dump_op_short(o); Perl_deb("\n"));
 
-	    codeseq->xcodeseq_instructions[idx].instr_ppaddr = PL_ppaddr[o->op_type];
-	    codeseq->xcodeseq_instructions[idx].instr_op = o;
-	    codeseq->xcodeseq_instructions[idx].instr_arg = NULL;
-
-	    idx++;
-	    if (idx >= codeseq->xcodeseq_size) {
-		codeseq->xcodeseq_size += 128;
-		Renew(codeseq->xcodeseq_instructions, codeseq->xcodeseq_size, INSTRUCTION);
-	    }
+	    S_append_instruction(codeseq, bpp, o, o->op_type);
 
 	    /* Save other instruction for retrieval. */
             if (o->op_type == OP_ENTERTRY) {
-		S_append_branch_point(&bpp, cLOGOPo->op_other->op_next, &(cLOGOPo->op_other_instr));
+		S_append_branch_point(bpp, cLOGOPo->op_other->op_next, &(cLOGOPo->op_other_instr));
             }
             else if ((PL_opargs[o->op_type] & OA_CLASS_MASK) == OA_LOGOP) {
-		S_append_branch_point(&bpp, cLOGOPo->op_other, &(cLOGOPo->op_other_instr));
+		S_append_branch_point(bpp, cLOGOPo->op_other, &(cLOGOPo->op_other_instr));
 	    }
 	    else if (o->op_type == OP_SUBST) {
-		S_append_branch_point(&bpp, cPMOPo->op_pmreplrootu.op_pmreplroot, &(cPMOPo->op_pmreplroot_instr));
-		S_append_branch_point(&bpp, cPMOPo->op_pmstashstartu.op_pmreplstart, &(cPMOPo->op_pmreplstart_instr));
-		S_append_branch_point(&bpp, cPMOPo->op_next, &(cPMOPo->op_subst_next_instr));
+		S_append_branch_point(bpp, cPMOPo->op_pmreplrootu.op_pmreplroot, &(cPMOPo->op_pmreplroot_instr));
+		S_append_branch_point(bpp, cPMOPo->op_pmstashstartu.op_pmreplstart, &(cPMOPo->op_pmreplstart_instr));
+		S_append_branch_point(bpp, cPMOPo->op_next, &(cPMOPo->op_subst_next_instr));
 	    }
             else if ((PL_opargs[o->op_type] & OA_CLASS_MASK) == OA_LOOP) {
-		S_append_branch_point(&bpp, cLOOPo->op_lastop->op_next, &(cLOOPo->op_last_instr));
-		S_append_branch_point(&bpp, cLOOPo->op_nextop, &(cLOOPo->op_next_instr));
-		S_append_branch_point(&bpp, cLOOPo->op_redoop, &(cLOOPo->op_redo_instr));
+		S_append_branch_point(bpp, cLOOPo->op_lastop->op_next, &(cLOOPo->op_last_instr));
+		S_append_branch_point(bpp, cLOOPo->op_nextop, &(cLOOPo->op_next_instr));
+		S_append_branch_point(bpp, cLOOPo->op_redoop, &(cLOOPo->op_redo_instr));
 	    }
 	    else if (o->op_type == OP_GREPSTART || o->op_type == OP_MAPSTART) {
 		o = o->op_next;
 
-		S_append_branch_point(&bpp, cLOGOPo->op_other, &(cLOGOPo->op_other_instr));
+		S_append_branch_point(bpp, cLOGOPo->op_other, &(cLOGOPo->op_other_instr));
 	    }
 	    else if (o->op_type == OP_LAST) {
 		o = NULL;
 	    }
 	    else if (o->op_type == OP_UNSTACK) {
-		S_append_branch_point(&bpp, o->op_next,
+		S_append_branch_point(bpp, o->op_next,
 		    &(o->op_unstack_instr));
 		o = NULL;
 	    }
@@ -115,19 +106,43 @@ Perl_compile_op(pTHX_ OP* startop, CODESEQ* codeseq)
 		    kid = cUNOPx(kid)->op_first;			/* pass rv2gv */
 		    kid = cUNOPx(kid)->op_first;			/* pass leave */
 		    kid = kid->op_next;
-		    S_append_branch_point(&bpp, kid, &(o->op_unstack_instr));
+		    S_append_branch_point(bpp, kid, &(o->op_unstack_instr));
 		}
 	    }
 	    else if (o->op_type == OP_FORMLINE) {
-		S_append_branch_point(&bpp, cLISTOPo->op_first, &(o->op_unstack_instr));
+		S_append_branch_point(bpp, cLISTOPo->op_first, &(o->op_unstack_instr));
 	    }
 
 	    if (o)
 		o = o->op_next;
-	}
-	codeseq->xcodeseq_instructions[idx].instr_ppaddr = NULL;
-	idx++;
-	if (idx >= codeseq->xcodeseq_size) {
+    }
+}
+
+void
+Perl_compile_op(pTHX_ OP* startop, CODESEQ* codeseq)
+{
+    OP* o;
+
+    BRANCH_POINT_PAD bpp;
+
+    Newx(bpp.op_instrpp_list, 128, OP_INSTRPP);
+    bpp.idx = 0;
+    bpp.op_instrpp_compile = bpp.op_instrpp_list;
+    bpp.op_instrpp_append = bpp.op_instrpp_list;
+    bpp.op_instrpp_end = bpp.op_instrpp_list + 128;
+
+    PERL_ARGS_ASSERT_COMPILE_OP;
+    codeseq->xcodeseq_size = 12;
+    Renew(codeseq->xcodeseq_instructions, codeseq->xcodeseq_size, INSTRUCTION);
+
+    o = startop;
+
+    do {
+	S_add_op(codeseq, &bpp, o);
+
+	codeseq->xcodeseq_instructions[bpp.idx].instr_ppaddr = NULL;
+	bpp.idx++;
+	if (bpp.idx >= codeseq->xcodeseq_size) {
 	    codeseq->xcodeseq_size += 128;
 	    Renew(codeseq->xcodeseq_instructions, codeseq->xcodeseq_size, INSTRUCTION);
 	}
@@ -152,7 +167,7 @@ Perl_compile_op(pTHX_ OP* startop, CODESEQ* codeseq)
 	if (bpp.op_instrpp_compile >= bpp.op_instrpp_append)
 	    break;
 
-	bpp.op_instrpp_compile->instr_idx = idx;
+	bpp.op_instrpp_compile->instr_idx = bpp.idx;
 	o = bpp.op_instrpp_compile->op;
 	bpp.op_instrpp_compile++;
 
