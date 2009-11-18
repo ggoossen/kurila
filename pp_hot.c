7548029f7ba2fff31836c2b37415d2d45bf2044f
Gerard Goossen <gerard@ggoossen.net>
2009-11-18 23:11:26 +0100

    Add marktstack checking asserting op nextstate ops

--- pp_hot.c
+++ pp_hot.c
@@ -50,6 +50,7 @@ PP(pp_nextstate)
     dVAR;
     PL_curcop = (COP*)PL_op;
     TAINT_NOT;		/* Each statement is presumed innocent */
+    assert(PL_markstack_ptr == PL_markstack + cxstack[cxstack_ix].blk_oldmarksp); /* No stray PUSHMARKs */
     PL_stack_sp = PL_stack_base + cxstack[cxstack_ix].blk_oldsp;
     FREETMPS;
     return NORMAL;
