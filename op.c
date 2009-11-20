f4ff617b5f9549df48b07c3ab11c89f958a59545
Gerard Goossen <gerard@ggoossen.net>
2009-11-20 16:01:34 +0100

    fix context to eval propagation

--- op.c
+++ op.c
@@ -1216,6 +1216,7 @@ Perl_scalarvoid(pTHX_ OP *o)
     case OP_LIST:
     case OP_LEAVEGIVEN:
     case OP_LEAVEWHEN:
+    case OP_LEAVEEVAL:
 	for (kid = cLISTOPo->op_first; kid; kid = kid->op_sibling)
 	    scalarvoid(kid);
 	break;
@@ -1226,9 +1227,6 @@ Perl_scalarvoid(pTHX_ OP *o)
     case OP_FOREACH:
 	for (kid = cLISTOPo->op_first->op_sibling->op_sibling; kid; kid = kid->op_sibling)
 	    scalarvoid(kid);
-	break;
-    case OP_LEAVEEVAL:
-	scalarkids(o);
 	break;
     case OP_SCALAR:
 	return scalar(o);
